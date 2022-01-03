5bff1748b5879f22713b0f579a5985e8a5267fc6
faslash <bicadete@hotmail.com>
2022-01-03 16:29:53 -0300

    Correções nas importações dos arquivos de testes

--- src/modules/accounts/useCases/createUser/CreateUserUseCase.ts
+++ src/modules/accounts/useCases/createUser/CreateUserUseCase.ts
@@ -14,7 +14,7 @@ class CreateUserUseCase {
     private usersRepository: IUsersRepository
   ) { }
 
-  async execute({ name, username, email, password, driver_license }: ICreateUserDTO): Promise<void> {
+  async execute({ name, email, password, driver_license }: ICreateUserDTO): Promise<void> {
 
     const userAlreadyExists = await this.usersRepository.findByEmail(email);
 
@@ -25,7 +25,7 @@ class CreateUserUseCase {
     const passwordHash = await hash(password, 8);
 
     await this.usersRepository.create({
-      name, username, email, password: passwordHash, driver_license
+      name, email, password: passwordHash, driver_license
     })
 
   }
